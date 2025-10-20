693c51971b41bcfce7ea9ea3cb513fcf11712ccc
StivenF0 <stivenfonseca201@gmail.com>
2025-10-20 15:00:37 -0300

    feat: add base structure for backend

---
+++ backend/src/main/java/com/subman/submanapi/SubManApplication.java
@@ -0,0 +1,13 @@
+package com.subman.submanapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubManApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubManApplication.class, args);
+	}
+
+}
