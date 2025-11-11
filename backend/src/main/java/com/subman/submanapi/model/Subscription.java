f17e0e120f97eae0b05187ee0351d3a6238e43e2
Stiven Felipe <stivenfonseca201@gmail.com>
2025-11-11 20:48:21 -0300

    refactor: change implementation of Subscription model

--- backend/src/main/java/com/subman/submanapi/model/Subscription.java
+++ backend/src/main/java/com/subman/submanapi/model/Subscription.java
@@ -1,11 +1,12 @@
 package com.subman.submanapi.model;
 
+import com.subman.submanapi.enums.BillingCycle;
 import com.subman.submanapi.enums.Category;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import java.math.BigDecimal;
-import java.util.Date;
+import java.time.LocalDate;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -14,10 +15,11 @@ import java.util.List;
 @NoArgsConstructor
 public class Subscription {
     private Long id;
+    private Long userId;
     private String nome;
     private BigDecimal valor;
     private Category categoria;
-    private Date vencimento;
-    private User usuario;
-    private List<Subscription> historico = new LinkedList<>();
+    private BillingCycle cicloCobranca;
+    private LocalDate vencimento;
+    private List<String> historicoPagamentos = new LinkedList<>();
 }
